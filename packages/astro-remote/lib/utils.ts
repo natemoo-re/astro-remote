edf3200b12a8f09f6c41be38661216373b1d1ff9
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-26 14:02:02 -0800

    okay some resolutions... not the best but it is working

--- packages/astro-remote/lib/utils.ts
+++ packages/astro-remote/lib/utils.ts
@@ -44,15 +44,14 @@ function getIndent(ln: string): string {
 }
 
 export function dedent(str: string): string {
-	const lns = str.replace(/^[\r\n]+/, "").split("\n");
-	let indent = getIndent(lns[0]);
-	if (indent.length === 0 && lns.length > 1) {
-		indent = getIndent(lns[1]);
-	}
-	return lns
-		.map((ln) => (ln.startsWith(indent) ? ln.slice(indent.length) : ln))
-		.map((ln, i, { length }) => (i === length - 1 ? ln.trim() : ln))
-		.join("\n");
+    const lns = str.replace(/^[\r\n]+/, "").split("\n");
+    let indent = getIndent(lns[0]);
+    if (indent.length === 0 && lns.length > 1) {
+        indent = getIndent(lns[1]);
+    }
+    return lns
+        .map((ln) => (indent.length > 1 && ln.startsWith(indent) ? ln.slice(indent.length) : ln))
+        .join("\n");
 }
 
 export interface HTMLOptions {
